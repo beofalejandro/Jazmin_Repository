14a068972dd539b8357f89ec9945356784a2227c
Alejandro Barrientos <112975406+beofalejandro@users.noreply.github.com>
2024-05-05 18:40:08 -0600

    Definitive

---
+++ src/scripts/index.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+
+// ANIMATIONS WITH USER SCROLL
+sr.reveal('.objective', {
+    duration: 1500,
+    origin: 'bottom'
+
+});
+
+sr.reveal('.section_2', {
+    duration: 1500,
+    origin: 'bottom'
+});
+
+sr.reveal('.section_3', {
+    duration: 1500,
+    origin: 'bottom'
+});
+
